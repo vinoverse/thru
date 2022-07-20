40fc1d52ec31214058bf74be3e49fcd2eeb1c3b2
jungdal-jeon <jungdal.jeon@metavin.co.kr>
2022-07-20 10:54:02 +0900

    NFT loading

--- frontend/src/pages/NFTList.js
+++ frontend/src/pages/NFTList.js
@@ -4,6 +4,7 @@ import { useWeb3React } from '@web3-react/core';
 import { Row, Button } from 'react-bootstrap';
 import NFTItem from '../component/NFTItem';
 import { Navigate } from 'react-router-dom';
+import { SpinnerModal } from '../component/SpinnerModal';
 
 
 const NFTList = (props) => {
@@ -11,9 +12,13 @@ const NFTList = (props) => {
     const [nfts, setNfts] = useState([]);
     const {apiDomain} = props;
 
+    const [spinner, setSpinner] = useState(false);
+
     useEffect(() => {   
+        setSpinner(true);
         fetch(apiDomain + "/api/user/nfts/" + account).then((res) => res.json()).then((res) => {
           setNfts(res["result"]);
+          setSpinner(false);
         });
     }, [apiDomain]);
 
@@ -22,6 +27,7 @@ const NFTList = (props) => {
             {account
             ?<div>
               <Button variant="dark" onClick={deactivate}>Disconnect</Button>
+              <SpinnerModal show={spinner} onHide={() => setSpinner(false)} />
               <Row xs={1} md={2} className="g-4">
                 {nfts.map((nft) => ( 
                     <NFTItem key={nft.name} nft={nft}/>
