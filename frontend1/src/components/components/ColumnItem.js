c75989a33097f6b1d70e345d3240375403f1850c
jungdal-jeon <jungdal.jeon@metavin.co.kr>
2022-08-11 15:06:43 +0900

    style change

--- frontend1/src/components/components/ColumnItem.js
+++ frontend1/src/components/components/ColumnItem.js
@@ -38,12 +38,12 @@ const ColumnItem = (props) => {
                         <span>
                             <h4>{name}</h4>
                         </span>
-                        {eventList 
-                        ? <div className="nft__item_like">
-                            <i className="fa fa-ticket"></i><span>{eventList.length}</span>
+                        <div className="nft__item_like">
+                            {eventList 
+                            ?<><i className="fa fa-ticket"></i><span>{eventList.length}</span></>
+                            :<><i className="fa"></i><span></span></>
+                            }
                         </div>
-                        :<div></div>
-                        }
                     </div> 
                 </div>
             </div>
